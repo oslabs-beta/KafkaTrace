7ac6f785ded2075c6cb631e8eb8ba934e84c3584
Alston Nguyen <77164621+alstonnguyen@users.noreply.github.com>
2023-07-22 11:39:51 -0700

    Revert "deleted files, only test files to merge to dev"

---
+++ src/server/test-services/consumer-service.tsx
@@ -0,0 +1,26 @@
+const { Kafka } = require('kafkajs');
+
+const consumeKafka = async () => {
+    const kafka = new Kafka({
+        clientId: 'email-service-client',
+        brokers: ['localhost:9092']
+    })
+
+    const consumer = kafka.consumer({groupId: 'email-group'});
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'send-email'});
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log('received message from kafka', {
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+consumeKafka();
+
+console.log('Consumer-service started')
