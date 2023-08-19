3dc7cefa86c61c441de7f0180463dca5455a5c41
Alston Nguyen <alstonnguyen@gmail.com>
2023-08-19 16:11:29 -0700

    modified layout and register page for mobile

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -41,8 +41,8 @@ export default function Register() {
         <title>Register</title>
       </Head>
 
-      <section className='p-12 my-24 mx-96 rounded-lg text-center space-y-6 bg-#fbfbfb w-half'>
-        <div className='max-w-4xl mx-auto text-center'>
+      <section className='p-12 my-24 mx-15 rounded-lg text-center space-y-6 bg-#fbfbfb w-full md:full'>
+        <div className='mx-auto text-center'>
           <h2 className='text-3xl font-bold mb-2 text-black'>Register</h2>
           <p className='text-lg text-black mb-5'>
             We need a little information to register you!
