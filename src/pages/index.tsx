2d54e0d879ed1692fc17e52891eefa3f49a246e5
Wai San Gu <waisangu@gmail.com>
2023-08-24 21:41:07 -0400

    additional code snippets added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import MainBanner from '../components/MainBanner';
-import InstallComponent from '../components/InstallComponent';
 import CodeSnippet from '../components/CodeSnippet';
 import DemoGif from '../components/DemoGif';
 import Features from '../components/Features';
 import Teams from '../components/Teams';
+
+const npmCode = 'npm install kafkatrace'
+
+const composerCode = `import { composer } from 'kafkatrace';
+composer();`
+
+const tracerCode = `import { tracer } from 'kafkatrace';
+tracer('[Service Name]');`
 
 export default function Root() {
   return (
     <div className='m-0 min-h-screen'>
       <Navbar />
         <MainBanner />
-        <InstallComponent />
-        <CodeSnippet />
+        <div className='flex flex-col'>
+          <h2>Get Started</h2>
+          <p>1. Install npm package to your project</p>
+          <CodeSnippet code={npmCode}/>
+          <p>2.Docker compose up the preconfigurd containers</p>
+          <CodeSnippet code={composerCode}/>
+          <p>3.Add to each of your services and replace the service name as desired</p>
+          <CodeSnippet code={tracerCode}/>
+
+        </div>
         <DemoGif />
         <Features />
         <Teams />
