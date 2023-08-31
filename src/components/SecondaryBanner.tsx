1cf28f32d05b478f3b693bb9f8e3798ff02d8d2c
Wai San Gu <waisangu@gmail.com>
2023-08-30 23:12:05 -0400

    get started justify center fixed

--- src/components/SecondaryBanner.tsx
+++ src/components/SecondaryBanner.tsx
@@ -14,7 +14,7 @@ export default function SecondaryBanner () {
     return (
         <div className='flex flex-col md:flex-wrap md:flex-row md:mx-48'>
             <div className='px-4 pt-4 md:p-4 font-akkurat font-medium text-black text-xl md:text-3xl'>Get Started</div>
-            <div className='md:flex md:flex-wrap md:flex-row md:justify-center 2xl:mt-12 md:mb-4 px-4 md:p-4'>
+            <div className='md:flex md:flex-wrap md:flex-row md:justify-between md:items-start 2xl:mt-12 md:mb-4 px-4 md:p-4'>
                 <div className='flex flex-col flex-[0_0_45%] sm:mx-4'>
                     <p className='font-akkurat text-black my-4'>1. Install npm package</p>
                     <CodeSnippet code={npmCode}/>
@@ -29,7 +29,7 @@ export default function SecondaryBanner () {
                     </p>
                 </div>
             </div>
-            <div className='flex flex-col p-4 flex-[0_0_100%]'>
+            <div className='flex flex-col justify-center p-4 flex-[0_0_100%]'>
                 <h2 className='font-akkurat font-medium text-black text-xl md:text-3xl'>Demo</h2>
                 <DemoGif />
             </div>
