9c4ba59b936d0d43cb148aa0fd0d146134d7270c
Wai San Gu <waisangu@gmail.com>
2023-11-20 14:40:48 -0500

    added border to next images

--- src/components/SecondaryBanner.tsx
+++ src/components/SecondaryBanner.tsx
@@ -10,6 +10,14 @@ composer();`
 const tracerCode = `import { tracer } from 'kafkatrace';
 tracer('[Service Name]');`
 
+const imageStyle = {
+    width: '100%', 
+    height: '100vh', 
+    position: 'relative',
+    border: '1px solid #A9A9A9',
+    borderRadius: '0.5rem'
+}
+
 export default function SecondaryBanner () {
     return (
         <div className='flex flex-col md:flex-wrap md:flex-row min-[768px]:mx-8 min-[1152px]:mx-32 2xl:mx-48'>
@@ -37,15 +45,14 @@ export default function SecondaryBanner () {
                 <DemoGif />
             </div>
             <div className='flex flex-col px-4 md:px-16 max-h-48 md:max-h-96 flex-[0_0_100%] md:flex-[0_0_50%] order-1'>
-                <Image
-                src="/assets/jaeger-ui.png"
-                alt='Jaeger UI'
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: 'auto' }}
-                className='rounded-lg'
-                />
+                <div style={imageStyle}>
+                    <Image
+                    src="/assets/jaeger-ui.png"
+                    alt='Jaeger UI'
+                    layout='fill'
+                    className='rounded-lg'
+                    />
+                </div>
             </div>
             <div className='flex flex-col p-8 md:p-16 md:min-h-96 flex-[0_0_100%] md:flex-[0_0_50%] font-akkurat font-medium text-black text-xl md:text-3xl text-center italic m-auto order-2'>
                 Pinpoint bottlenecks in your streaming pipelines to quickly troubleshoot issues that may arise.
@@ -54,26 +61,24 @@ export default function SecondaryBanner () {
                 Obtain visibility to impacts from your Apache Kafka Producer and Consumer Clients.
             </div>
             <div className='flex flex-col px-4 md:px-16 max-h-48 md:max-h-96 flex-[0_0_100%] md:flex-[0_0_50%] order-3 md:order-4'>
-                <Image
-                src="/assets/zipkin-ui.png"
-                alt='Jaeger UI'
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: 'auto' }}
-                className='rounded-lg'
-                />
+                <div style={imageStyle}>
+                    <Image
+                    src="/assets/zipkin-ui.png"
+                    alt='Jaeger UI'
+                    layout='fill'
+                    className='rounded-lg'
+                    />
+                </div>
             </div>
             <div className='flex flex-col px-4 md:px-16 max-h-48 md:max-h-96 flex-[0_0_100%] md:flex-[0_0_50%] order-5'>
-                <Image
-                src="/assets/prometheus-ui.png"
-                alt='Jaeger UI'
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: 'auto' }}
-                className='rounded-lg'
-                />
+                <div style={imageStyle}>
+                    <Image
+                    src="/assets/prometheus-ui.png"
+                    alt='Jaeger UI'
+                    layout='fill'
+                    className='rounded-lg'
+                    />
+                </div>
             </div>
             <div className='flex flex-col p-8 md:p-16 md:min-h-96 flex-[0_0_100%] md:flex-[0_0_50%] font-akkurat font-medium text-black text-xl md:text-3xl text-center italic m-auto order-6'>
                 Choose from popular and established open source monitoring user interfaces.
