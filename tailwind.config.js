08f2b6a31a4cf192d5288aef951ba4bba4ddaeb3
Wai San Gu <waisangu@gmail.com>
2024-02-20 11:59:05 -0500

    fixed dark mode in windows chrome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['{pages,src}/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'false',
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
